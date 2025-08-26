ca2ed88590f2153038c5d97aeae5e3d9320c80fd
pmbjojo <jourdamj@gmail.com>
2025-08-26 15:14:15 +0200

    fix resend

--- src/app/api/send_confirmation/route.ts
+++ src/app/api/send_confirmation/route.ts
@@ -1,42 +1,33 @@
-import { NextResponse } from "next/server";
 import { Resend } from "resend";
+import { EmailTemplate } from "@/components/email";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(req: Request) {
-  try {
-    const { email, name, date, time } = await req.json();
+interface EmailRequest {
+	email: string;
+	name: string;
+	date: string;
+	time: string;
+}
 
-    if (!email || !name || !date || !time) {
-      return NextResponse.json(
-        { success: false, error: "Champs requis manquants." },
-        { status: 400 }
-      );
-    }
+export async function POST(request: Request) {
+	const { email, name, date, time } = (await request.json()) as EmailRequest;
+	try {
+		const { data, error } = await resend.emails.send({
+			from: "Acme <onboarding@resend.dev>",
+			to: [email],
+			subject: "Hello world",
+			react: EmailTemplate({ name, date, time }),
+		});
 
-    await resend.emails.send({
-      from: "onboarding@resend.dev", // ou un domaine validé chez Resend
-      to: email, // email du client
-      subject: "Confirmation de votre réservation - Restaurant Joséphine",
-      html: `
-        <h2>Bonjour ${name},</h2>
-        <p>Votre réservation au <b>restaurant Joséphine</b> est confirmée :</p>
-        <ul>
-          <li>Date : ${date}</li>
-          <li>Heure : ${time}</li>
-        </ul>
-        <p>Nous avons hâte de vous accueillir 🍷🍴</p>
-        <p>À bientôt,</p>
-        <p><b>L'équipe Joséphine</b></p>
-      `,
-    });
+		if (error) {
+			console.log("send email error", error);
+			return Response.json({ error }, { status: 500 });
+		}
 
-    return NextResponse.json({ success: true });
-  } catch (err) {
-    console.error("Erreur envoi email:", err);
-    return NextResponse.json(
-      { success: false, error: "Erreur lors de l'envoi de l'email." },
-      { status: 500 }
-    );
-  }
+		return Response.json(data);
+	} catch (error) {
+		console.log("try catch", error);
+		return Response.json({ error }, { status: 500 });
+	}
 }
