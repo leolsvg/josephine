67a7c8048d4a676151895f9923232ba4351c6994
leolsvg <leorenouf2@icloud.com>
2025-08-29 18:02:48 +0200

    refactor(send_confirmation): improve email sending logic and formatting

--- src/app/api/send_confirmation/route.ts
+++ src/app/api/send_confirmation/route.ts
@@ -4,30 +4,31 @@ import { EmailTemplate } from "@/components/email";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 interface EmailRequest {
-	email: string;
-	name: string;
-	date: string;
-	time: string;
+  email: string;
+  name: string;
+  date: string;
+  time: string;
 }
 
 export async function POST(request: Request) {
-	const { email, name, date, time } = (await request.json()) as EmailRequest;
-	try {
-		const { data, error } = await resend.emails.send({
-			from: "Acme <onboarding@resend.dev>",
-			to: [email],
-			subject: "Hello world",
-			react: EmailTemplate({ name, date, time }),
-		});
+  const { email, name, date, time } = (await request.json()) as EmailRequest;
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "Restaurant Josephine <no-reply@josephine-cherbourg.fr>",
+      to: [email],
+      bcc: ["leorenouf2@icloud.com", "s.scelles@gmail.com"],
+      subject: "Réservation confirmée - Restaurant Josephine",
+      react: EmailTemplate({ name, date, time }),
+    });
 
-		if (error) {
-			console.log("send email error", error);
-			return Response.json({ error }, { status: 500 });
-		}
+    if (error) {
+      console.log("send email error", error);
+      return Response.json({ error }, { status: 500 });
+    }
 
-		return Response.json(data);
-	} catch (error) {
-		console.log("try catch", error);
-		return Response.json({ error }, { status: 500 });
-	}
+    return Response.json(data);
+  } catch (error) {
+    console.log("try catch", error);
+    return Response.json({ error }, { status: 500 });
+  }
 }
