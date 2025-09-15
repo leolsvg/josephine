2727f965a013b416d7a5ff576aa5fe26feae3f2b
pmbjojo <jourdamj@gmail.com>
2025-09-15 23:42:18 +0200

    chore: install new deps, remove old ones and replace eslint by biome

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,27 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import Script from "next/script";
+import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import type { Metadata } from "next";
+import { geistMono, geistSans } from "@/styles/fonts";
 
 export const metadata: Metadata = {
-  title: "Joséphine",
-  description:
-    "Découvrez Joséphine, restaurant de bistronomie au cœur de Cherbourg. Cuisine raffinée, produits frais et ambiance conviviale. Réservez en ligne dès maintenant.",
+	title: "Joséphine",
+	description:
+		"Découvrez Joséphine, restaurant de bistronomie au cœur de Cherbourg. Cuisine raffinée, produits frais et ambiance conviviale. Réservez en ligne dès maintenant.",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="fr">
-      <body>
-        {children}
-        <Analytics />
-
-        <Script src="/js/some-client.js" strategy="afterInteractive" />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="fr" suppressHydrationWarning className="scroll-smooth">
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+			>
+				{children}
+				<Analytics />
+			</body>
+		</html>
+	);
 }
