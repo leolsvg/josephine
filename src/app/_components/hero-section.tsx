55b28c1b816d5a69bb6a9e3587c1f206c4b32826
pmbjojo <jourdamj@gmail.com>
2025-09-23 17:58:00 +0200

    fix: hero section padding and text size on mobile

--- src/app/_components/hero-section.tsx
+++ src/app/_components/hero-section.tsx
@@ -4,10 +4,10 @@ export function HeroSection() {
       <section className="bg-cover bg-center bg-no-repeat h-dvh bg-[url('/restaurant/bar.jpeg')] relative">
         <div className="absolute inset-0 bg-black opacity-50 z-0" />
         <div className="relative  flex flex-col justify-center items-center size-full text-white gap-6 z-10">
-          <h1 className="text-6xl font-bold leading-tight text-center">
+          <h1 className="text-5xl sm:text-6xl font-bold leading-tight text-center px-6">
             Bienvenue chez Joséphine
           </h1>
-          <div className="text-xl text-center max-w-2xl">
+          <div className="text-lg sm:text-xl text-center max-w-2xl px-6">
             Joséphine est un restaurant de bistronomie situé à Cherbourg, nous
             utilisons des produits locaux et de saison pour vous offrir une
             expérience culinaire unique.
