8ae848cd95e1d74e487c62d68553efb145505d1c
pmbjojo <jourdamj@gmail.com>
2025-09-28 22:45:30 +0200

    refactor: clean props

--- src/components/form/date-time-field.tsx
+++ src/components/form/date-time-field.tsx
@@ -1,42 +1,19 @@
+import type { ComponentProps } from "react";
 import { DateTimeInput } from "@/components/form/date-time-input";
 import { FieldErrors } from "@/components/form/field-errors";
 import { FormField } from "@/components/form/form-field";
 import type { FieldProps } from "@/components/form/types";
 import { Label } from "@/components/ui/label";
 
-type DateTimeFieldProps = {
-  disabled: (date: Date) => boolean;
-  timeSlots: (date: Date) => string[][];
-  date: string | undefined;
-  time: string | undefined;
-  onDateChange: (date: string | undefined) => void;
-  onTimeChange: (time: string | undefined) => void;
-} & FieldProps;
+type DateTimeFieldProps = ComponentProps<typeof DateTimeInput> & FieldProps;
 
-export function DateTimeField({
-  label,
-  disabled,
-  timeSlots,
-  date,
-  onDateChange,
-  onTimeChange,
-  time,
-  id,
-}: DateTimeFieldProps) {
+export function DateTimeField({ label, ...props }: DateTimeFieldProps) {
   return (
     <FormField>
-      <Label className="mb-3" htmlFor={id}>
+      <Label className="mb-3" htmlFor={props.id}>
         {label}
       </Label>
-      <DateTimeInput
-        id={id}
-        disabled={disabled}
-        timeSlots={timeSlots}
-        date={date}
-        time={time}
-        onTimeChange={onTimeChange}
-        onDateChange={onDateChange}
-      />
+      <DateTimeInput {...props} />
       <FieldErrors />
     </FormField>
   );
