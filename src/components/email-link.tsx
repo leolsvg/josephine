215584f3f464e1edacb9685a92c401cf93c663dc
pmbjojo <jourdamj@gmail.com>
2025-10-02 05:25:29 +0200

    style: remove email link padding

--- src/components/email-link.tsx
+++ src/components/email-link.tsx
@@ -2,7 +2,7 @@ import { Button } from "./ui/button";
 
 export function EmailLink({ email }: { email: string }) {
   return (
-    <Button variant="link" size="sm" asChild>
+    <Button variant="link" size="sm" asChild className="p-0">
       <a href={`mailto:${email}`}>{email}</a>
     </Button>
   );
