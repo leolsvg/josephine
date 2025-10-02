38e94ab2edc0c6807dd490989e1e2c1b0c746a9e
pmbjojo <jourdamj@gmail.com>
2025-10-02 05:26:44 +0200

    style: display raw phone if formatting fails and remove padding

--- src/app/admin/bookings/_components/data-table/columns/phone-link.tsx
+++ src/app/admin/bookings/_components/data-table/columns/phone-link.tsx
@@ -2,9 +2,9 @@ import { formatPhoneNumberIntl } from "react-phone-number-input";
 import { Button } from "@/components/ui/button";
 
 export function PhoneLink({ phone }: { phone: string }) {
-  const formattedPhone = formatPhoneNumberIntl(phone);
+  const formattedPhone = formatPhoneNumberIntl(phone) || phone;
   return (
-    <Button variant="link" size="sm" asChild>
+    <Button variant="link" size="sm" asChild className="p-0">
       <a href={`tel:${phone}`}>{formattedPhone}</a>
     </Button>
   );
