bbac48b26697a687c30d250ce612378647a333f1
pmbjojo <jourdamj@gmail.com>
2025-10-02 08:51:49 +0200

    fix: make db singleton because of issues with multiple connections and timeouts in dev

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,8 +1,25 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import { env } from "@/lib/env";
+import * as schema from "./schema";
 
-// Disable prefetch as it is not supported for "Transaction" pool mode
-export const client = postgres(env.DATABASE_URL, { prepare: false });
-export const db = drizzle(client, { casing: "snake_case" });
+class Database {
+  private static instance: Database;
+  public db;
+
+  private constructor() {
+    // Disable prefetch as it is not supported for "Transaction" pool mode
+    const client = postgres(env.DATABASE_URL, { prepare: false });
+    this.db = drizzle(client, { casing: "snake_case", schema });
+  }
+
+  public static getInstance() {
+    if (!Database.instance) {
+      Database.instance = new Database();
+    }
+    return Database.instance;
+  }
+}
+
+export const { db } = Database.getInstance();
 export type DB = typeof db;
