19e1847849c4e4916eb2cbffd291aa97356f9d95
pmbjojo <jourdamj@gmail.com>
2025-10-04 19:21:48 +0200

    chore: remove unused components

--- src/components/form/form-field.tsx
+++
@@ -1,5 +0,0 @@
-import type { ReactNode } from "react";
-
-export function FormField({ children }: { children: ReactNode }) {
-  return <div className="grid w-full items-center">{children}</div>;
-}
