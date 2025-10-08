78b453121ecaecb23368265f5c0fe5728cb1dd1e
pmbjojo <jourdamj@gmail.com>
2025-10-08 19:04:55 +0200

    chore: remove form devtools

--- src/components/devtools.tsx
+++ src/components/devtools.tsx
@@ -1,13 +1,7 @@
 "use client";
 
-import { FormDevtools } from "@tanstack/react-form-devtools";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export function Devtools() {
-  return (
-    <>
-      <ReactQueryDevtools />
-      <FormDevtools />
-    </>
-  );
+  return <ReactQueryDevtools />;
 }
