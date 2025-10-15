3efbecf6c1cfe64b907083d4f2a12d2eb8bfea55
pmbjojo <jourdamj@gmail.com>
2025-10-15 04:43:54 +0200

    style: add header border to improve contrast with white background and make the maps and phone buttons transparent on desktop

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -31,16 +31,16 @@ export const navigationItems: NavItem[] = [
 export function Header() {
   return (
     <div className="fixed top-0 z-20 w-full p-2">
-      <header className="flex md:grid md:grid-cols-3 p-3 items-center justify-between bg-transparent md:bg-white/80 md:backdrop-blur-md md:shadow-md relative rounded-md">
+      <header className="flex md:grid md:grid-cols-3 p-3 items-center justify-between bg-transparent md:bg-white/80 md:backdrop-blur-md md:shadow-md relative rounded-md md:border-gray-100 md:border">
         <div>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button
                 variant="outline"
                 size="icon"
-                className="p-1 shadow-md block md:hidden"
+                className="shadow-md block md:hidden"
               >
-                <JosephineIcon className="size-6" />
+                <JosephineIcon className="p-1" />
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent>
@@ -66,8 +66,8 @@ export function Header() {
           <Button
             asChild
             size="sm"
+            className="shadow-md md:bg-transparent md:border-0 md:shadow-none rounded w-8 xl:w-auto"
             variant="outline"
-            className="shadow-md md:shadow-none w-8 xl:w-auto"
           >
             <a href={`tel:${Josephine.phone}`}>
               <Phone />
@@ -77,8 +77,8 @@ export function Header() {
           <Button
             asChild
             size="icon-sm"
+            className="shadow-md md:bg-transparent md:border-0 md:shadow-none md:mr-1 rounded"
             variant="outline"
-            className="shadow-md md:shadow-none"
           >
             <a
               href={Josephine.mapsUrl}
