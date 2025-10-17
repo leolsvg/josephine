a79a30c4b6a1cd3913ae739136ddc8339e79c299
pmbjojo <jourdamj@gmail.com>
2025-10-17 08:28:48 +0200

    fix: ssr hydration for BookingsAreaChart

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -8,12 +8,17 @@ import { StatusPieChart } from "./bookings/_components/sidebar/status-pie-chart"
 export default function DashboardPage() {
   const queryClient = createQueryClient();
   queryClient.prefetchQuery(trpc.bookings.get.queryOptions());
+  queryClient.prefetchQuery(
+    trpc.statistics.getBookingsByInterval.queryOptions(),
+  );
   return (
     <div>
       <h1 className="text-2xl font-bold mb-6">Tableau de bord</h1>
       <p>Bienvenue sur le tableau de bord administrateur.</p>
-      <BookingsAreaChart />
       <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense>
+          <BookingsAreaChart />
+        </Suspense>
         <Suspense>
           <StatusPieChart />
         </Suspense>
