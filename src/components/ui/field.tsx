8278853f46e994b4001050653e95a4e189c0047f
pmbjojo <jourdamj@gmail.com>
2025-10-17 08:36:15 +0200

    chore: lint

--- src/components/ui/field.tsx
+++ src/components/ui/field.tsx
@@ -83,6 +83,7 @@ function Field({
   ...props
 }: React.ComponentProps<"div"> & VariantProps<typeof fieldVariants>) {
   return (
+    // biome-ignore lint/a11y/useSemanticElements: shadcn code
     <div
       role="group"
       data-slot="field"
@@ -207,6 +208,7 @@ function FieldError({
       <ul className="ml-4 flex list-disc flex-col gap-1">
         {errors.map(
           (error, index) =>
+            // biome-ignore lint/suspicious/noArrayIndexKey: shadcn code
             error?.message && <li key={index}>{error.message}</li>,
         )}
       </ul>
