d20f1a1b5d1d005782c8b71d21e9fd27b9cfa8b5
Renouf Léo <leorenoufc@gmail.com>
2025-12-23 10:38:36 +0100

    fix: update lunch menu prices and reorder imports

--- src/app/menus/lunch/page.tsx
+++ src/app/menus/lunch/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import {
   Menu,
   MenuImage,
@@ -9,6 +8,7 @@ import {
 } from "@/feat/menus/components/menu";
 import { caller } from "@/lib/trpc/server";
 import { menuSectionTitles, priceIntl } from "@/lib/utils";
+import Image from "next/image";
 import TableImage from "../../../../public/restaurant/table.jpeg";
 
 export default async function LunchPage() {
@@ -28,9 +28,9 @@ export default async function LunchPage() {
       <Menu>
         <MenuTitle>Carte du déjeuner</MenuTitle>
         <ul className="font-bold text-[16px] sm:text-[28px] lg:text-[22px] text-black text-center lg:text-left leading-relaxed">
-          <li>Entrée + Plat ou Plat + Dessert {priceIntl.format(19)}</li>
-          <li>Entrée + Plat + Dessert {priceIntl.format(24)}</li>
-          <li>Plat seul {priceIntl.format(16)}</li>
+          <li>Entrée + Plat ou Plat + Dessert {priceIntl.format(24)}</li>
+          <li>Entrée + Plat + Dessert {priceIntl.format(28)}</li>
+          <li>Plat seul {priceIntl.format(18)}</li>
         </ul>
         {menu.map(([c, p]) => (
           <section key={c}>
